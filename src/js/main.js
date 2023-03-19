c5c82ed1e350ea03c0deab676d5465184b48b8a2
Leonid <leonid.spb2015@yandex.ru>
2023-03-19 16:58:12 +0300

    Верстка сайта Bright Lights

---
+++ src/js/main.js
@@ -0,0 +1,11 @@
+var ava1 = document.getElementById('ava-1');
+var ava2 = document.getElementById('ava-2');
+var ava3 = document.getElementById('ava-3')
+
+
+
+
+ava1.addEventListener('click', function() {
+ava2.classList.toggle=("ava-2--none");
+ava3.classList.toggle=("ava-3");
+} )
