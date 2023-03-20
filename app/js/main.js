fa637dd9c9bb739996bc41e1bef6a49f035e71c9
Leonid <leonid.spb2015@yandex.ru>
2023-03-20 21:56:44 +0300

    Добавил анимацию для social-icons , avatars-block

--- app/js/main.js
+++ app/js/main.js
@@ -1,1 +1,1 @@
-var ava1=document.getElementById("ava-1"),ava2=document.getElementById("ava-2"),ava3=document.getElementById("ava-3");ava1.addEventListener("click",function(){ava2.classList.toggle="ava-2--none",ava3.classList.toggle="ava-3"});
+var ava1=document.getElementById("ava-1"),ava2=document.getElementById("ava-2"),ava3=document.getElementById("ava-3");ava1.addEventListener("click",function(){ava2.classList.toggle("active--change")});
--- src/js/main.js
+++ src/js/main.js
@@ -6,6 +6,5 @@ var ava3 = document.getElementById('ava-3')
 
 
 ava1.addEventListener('click', function() {
-ava2.classList.toggle=("ava-2--none");
-ava3.classList.toggle=("ava-3");
+ava2.classList.toggle("active--change");
 } )
