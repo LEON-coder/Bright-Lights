a10978ffac7a5080b27585585fe4ff8911bd3dd3
Leonid <leonid.spb2015@yandex.ru>
2023-03-24 22:10:55 +0300

    Добавил проигрыватель в блок header и last-tracks

--- app/js/music-player.js
+++ app/js/music-player.js
@@ -1,28 +1,28 @@
 
 let audio = document.getElementById("audio");    // Берём элемент audio
 let time = document.querySelector(".time");      // Берём аудио дорожку
-let buttonPlay = document.querySelector(".button-play");   // Берём кнопку проигрывания
-let pauseButton = document.querySelector(".button-pause");
+let buttonPlay = document.querySelector(".button-play");  // Берём кнопку проигрывания
+let timePlaying = document.querySelector(".time-playing");
+let NextTrack = document.querySelector(".last-tracks__block-track")
 
-
-let playlist = [
-	'../../app/music/3LAU_Bright_Lights_-_How_You_Love_Me_48268182.mp3',
+let playlist = [	
 	'../../app/music/Benny_Benassi_and_Pink_Is_Punk_feat_Bright_Lights_-_Ghost_Original_Radio_Edit_67157669.mp3',
 	'../../app/music/Bright_Lights_Kaleena_Zanders_Kandy_-_War_For_Love_70971022.mp3',
-	'../../app/music/Dyro_Hardwell_Bright_Lights_-_Never_Say_Goodbye_55725060.mp3'
+	'../../app/music/Dyro_Hardwell_Bright_Lights_-_Never_Say_Goodbye_55725060.mp3',
+	'../../app/music/Zedd_Bright_Lights_-_Follow_You_Down_48263215.mp3',
 ];
 
-let track; // Переменная с индексом трека
+let treck; // Переменная с индексом трека
 
 // Событие перед загрузкой страницы
 window.onload = function() {
-	track = 0; // Присваиваем переменной ноль
+	treck = 0; // Присваиваем переменной ноль
 }
 
 
-function switchTrack(numTrack) {
+function switchTreck(numTreck) {
 	// Меняем значение атрибута src
-	audio.src = '../../music/3LAU_Bright_Lights_-_How_You_Love_Me_48268182.mp3' + playlist[numTrack];
+	audio.src = '../../music/3LAU_Bright_Lights_-_How_You_Love_Me_48268182.mp3' + playlist[1];
 	// Назначаем время песни ноль
 	audio.currentTime = 0;
 	// Включаем песню
@@ -30,7 +30,11 @@ function switchTrack(numTrack) {
 }
 
 buttonPlay.addEventListener("click" , function() {
-	       audio.play(); // Запуск песни
+	if (audio.paused) {
+		audio.play();
+	} else {
+		audio.pause();
+	}	 
 			// Запуск интервала
 			audioPlay = setInterval(function() {
 		   // Получаем значение на какой секунде песня
@@ -41,17 +45,66 @@ buttonPlay.addEventListener("click" , function() {
 			time.style.width = (audioTime * 100) / audioLength + '%';
 			// Сравниваем, на какой секунде сейчас трек и всего сколько времени длится
             // И проверяем что переменная track меньше четырёх
-		if (audioTime == audioLength && track < 3) {
-			track++; // то увеличиваем переменную
-			switchTrack(track); // Меняем трек
+		if (audioTime == audioLength) {
+			treck++; // то увеличиваем переменную
+			switchTreck(treck); // Меняем трек
 			  // Иначе проверяем тоже самое, но переменная track больше или равна четырём
-		} else if (audioTime == audioLength && track >=3) {
-			track = 0; // То присваиваем treck ноль
-			switchTrack(track); // Меняем трек
+		} else if (audioTime == audioLength && treck >=6) {
+			treck = 0; // То присваиваем treck ноль
+			switchTreck(treck); // Меняем трек
 		}
 		}, 10)
+		
 	});
 
-	pauseButton.addEventListener("click" , function() {
-		audio.pause(); // Остановить трек
-	})
+
+            audio.addEventListener('timeupdate', function() {
+            timePlaying.innerHTML = secondsToTime(audio.currentTime);
+        }, false);
+
+
+		NextTrack.addEventListener("PlayNextTrack", function() {
+			buttonPlay
+		}  )
+
+  // рассчет отображаемого времени
+        function secondsToTime(time){
+             
+            var h = Math.floor(time / (60 * 60)),
+                dm = time % (60 * 60),
+                m = Math.floor(dm / 60),
+                ds = dm % 60,
+                s = Math.ceil(ds);
+            if (s === 60) {
+                s = 0;
+                m = m + 1;
+            }
+            if (s < 10) {
+                s = '0' + s;
+            }
+            if (m === 60) {
+                m = 0;
+                h = h + 1;
+            }
+            if (m < 10) {
+                m = '0' + m;
+            }
+            if (h === 0) {
+                fulltime = m + ':' + s;
+            } else {
+                fulltime = h + ':' + m + ':' + s;
+            }
+            return fulltime;
+        }	
+	
+	
+
+
+
+
+
+
+
+
+  
+	
