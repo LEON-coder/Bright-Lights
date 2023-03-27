e2f45d3b41b4722dc6cdb17428301db01deb6ec5
Leonid <leonid.spb2015@yandex.ru>
2023-03-27 20:44:37 +0300

    Корректировка отображения сайта и изображений

---
+++ app/js/bright-lights-slider.js
@@ -0,0 +1,14 @@
+let leftArrow = document.querySelector(".bright-lights-slider__left-block");
+let rightArrow = document.querySelector(".bright-lights-slider__right-block");
+let slider = document.querySelector(".bright-lights-slider__slider-block");
+
+function rotateSliderLeft() {
+  leftArrow
+}
+
+leftArrow.addEventListener("click" , rotateSliderLeft(){
+
+});
+
+
+
